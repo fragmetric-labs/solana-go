c7b49557e4e8d50a390781f0ca45121fec276660
taehoon <kth7024@naver.com>
2025-07-22 14:20:15 +0900

    Revert "fix: use Params() function in CallForInto and CallWithCallback"
    This reverts commit 92e32001c4b8c561a91de420d2db2bb2fa11ca95.

--- rpc/jsonrpc/jsonrpc.go
+++ rpc/jsonrpc/jsonrpc.go
@@ -383,8 +383,11 @@ func (client *rpcClient) CallForInto(
 ) error {
 	request := &RPCRequest{
 		Method:  method,
-		Params:  Params(params...),
 		JSONRPC: jsonrpcVersion,
+	}
+
+	if params != nil {
+		request.Params = params
 	}
 
 	rpcResponse, err := client.doCall(ctx, request)
@@ -407,8 +410,11 @@ func (client *rpcClient) CallWithCallback(
 ) error {
 	request := &RPCRequest{
 		Method:  method,
-		Params:  Params(params...),
 		JSONRPC: jsonrpcVersion,
+	}
+
+	if params != nil {
+		request.Params = params
 	}
 
 	return client.doCallWithCallbackOnHTTPResponse(
